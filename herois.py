808ac786717331e4774f0be5ecc254dc83521129
David Castanheira de Souza <2202571@SALAS.AULAS>
2023-11-01 21:25:57 -0300

    Inclusão dos arquivos para consultas no banco de dados

---
+++ herois.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
+engine = create_engine('sqlite:///rpg.db')
+class HeroiNaoExisteException(Exception):
+    pass
+
+#funções adicionais implementadas abaixo
+
+def heroi_existe(id_h):
+    with engine.connect() as con:  #conecta no meu banco de dados
+        #query com "buraco" com o nome heroi    
+        statement = text ('SELECT * FROM Heroi WHERE id = :heroi')
+        # :jogador -> buraco que vai ser preenchido quando eu chamar con.execute
+        # :jogador -> O ":" marca o buraco. Sem ":" nao tem buraco, e coisas estranhas vao acontecer
+       
+        rs = con.execute(statement, heroi=id_h) #e usei esse buraco
+        herois = rs.fetchall()                 #pega todos os resultados
+        if herois == []:                       #se nao tinha nenhuma linha
+            return False
+        return True
+
+def consultar_heroi(id_h):
+    with engine.connect() as con: 
+        statement = text ("""SELECT * FROM Heroi WHERE id = :heroi""")
+        rs = con.execute(statement, heroi=id_h)
+        herois = rs.fetchall()
+        if herois == []:
+            raise HeroiNaoExisteException
+        return dict(herois[0])
+
